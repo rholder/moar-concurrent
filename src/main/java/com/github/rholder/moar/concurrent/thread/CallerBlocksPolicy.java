2ff299332bbd65daabf330beea903ed79da16cd8
Ray Holder <ray@blacklocus.com>
2013-05-22 00:49:11 -0500

    adding CallerBlocksPolicy to prevent the submit thread from starving a task queue when dealing with long running tasks in ThreadPoolExecutor's

---
+++ src/main/java/com/github/rholder/moar/concurrent/thread/CallerBlocksPolicy.java
@@ -0,0 +1,41 @@
+package com.github.rholder.moar.concurrent.thread;
+
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * This {@link RejectedExecutionHandler} blocks the current thread until the
+ * backing queue for a {@link ThreadPoolExecutor} can accept another task.  This
+ * becomes useful for situations where tasks are typically long running or
+ * unknown and it is undesirable to block the task submission thread for the
+ * entire length of one of these tasks (which can easily happen if using the
+ * {@link java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy}). Provided
+ * your queue of choice for the {@link ThreadPoolExecutor} is bounded, you'll be
+ * able to continue submitting tasks up until the point where the queue is full
+ * and then resume the submitting of new tasks immediately when the queue can
+ * accept more without having to wait on an existing task in the submitting
+ * thread to finish.
+ */
+public class CallerBlocksPolicy implements RejectedExecutionHandler {
+
+    /**
+     * Instead of throwing away the rejected task, put it back onto the queue
+     * that the {@link ThreadPoolExecutor} is using, effectively blocking
+     * execution until the put() succeeds or results in an
+     * {@link InterruptedException} which is wrapped and rethrown as
+     * a {@link RejectedExecutionException}.
+     *
+     * @param r        the runnable task requested to be executed
+     * @param executor the executor attempting to execute this task
+     */
+    @Override
+    public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+        try {
+            // block on the internal queue
+            executor.getQueue().put(r);
+        } catch (InterruptedException e) {
+            throw new RejectedExecutionException("Unexpected InterruptedException", e);
+        }
+    }
+}
